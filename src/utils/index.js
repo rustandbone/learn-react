c9b026f9a17381bd2f2af93e76ce224b18f65f9e
rustandbone <dlehdgh14@naver.com>
2023-08-11 16:04:22 +0900

    getNode 유틸 함수 생성

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export { default as numberWithComma } from './numberWithComma';
 export { default as getRandomMinMax, getRandom } from './getRandomMinMax';
 export * from './currency';
+export * from './getNode';
