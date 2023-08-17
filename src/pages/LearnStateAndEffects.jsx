df2a4b15c58a0143eda12be3ccaf108cba3a57d3
rustandbone <dlehdgh14@naver.com>
2023-08-17 16:18:50 +0900

    0817 목  pocketbase 연결

--- src/pages/LearnStateAndEffects.jsx
+++ src/pages/LearnStateAndEffects.jsx
@@ -1,75 +1,55 @@
-import { useState } from "react";
-
-const fontWeightList = (Array(9).fill(1).map((n,i) => 100 * (i + 1)))
-
-const getFontWeight = (selectedFontWeight) => {
-  let fontWeight = '';
-
-  switch(selectedFontWeight) {
-    default: 
-    case 400:
-      fontWeight = 'font-normal';
-      break;
-    case 100:
-      fontWeight = 'font-thin';
-      break;
-    case 200:
-      fontWeight = 'font-extralight';
-      break;
-    case 300:
-      fontWeight = 'font-light';
-      break;
-    case 500:
-      fontWeight = 'font-medium';
-      break;
-    case 600:
-      fontWeight = 'font-semibold';
-      break;
-    case 700:
-      fontWeight = 'font-bold';
-      break;
-    case 800:
-      fontWeight = 'font-extrabold';
-      break;
-    case 900:
-      fontWeight = 'font-black';
-  }
-
-  return fontWeight;
-}
+import Spinner from "@/components/Spinner";
+import { useEffect, useState } from "react";
 
 function LearnStateAndEffects() {
-  const [fontWeights] = useState(fontWeightList);
-  const [selectedFontWeight, setSelectedFontWeight] = useState(fontWeightList[3]);
+  //1. data
+  const [data, setData] = useState([]);
+  //2. isLoading
+  const [isLoading, setIsLoading] = useState(false);
+  //3. error
+  const [error, setError] = useState(null);
 
+  //side effect
+  //request data
+  useEffect(() => {
+    setIsLoading(true);
 
-  const fontWeight = getFontWeight(selectedFontWeight);
-  const handleChangeFontWeight = (index) => {
-    setSelectedFontWeight(fontWeightList[index])
+    //async await
+    async function fetchTodos() {
+      //fetch
+      const response = await fetch("http://127.0.0.1:3000/api/collections/todos/records")
+
+      if(!response.ok) {
+        // error handling
+        console.log(response.ok)
+      }
+
+      const data = await response.json();
+      setData(data)
+      setIsLoading(false);
+    }
+    //fetch or axios
+    fetchTodos();
+  }, [])
+
+  if(isLoading) {
+    return (
+        <Spinner size={120} className="absolute z-[9999] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+    )
   }
 
   return (
     <div className="m-10 flex flex-col gap-2 items-start" lang="en">
-      <h2 className={`text-indigo-600 font-suit text-2xl uppercase ${fontWeight}`}>
+      <h2 className={`text-indigo-600 font-suit text-2xl uppercase`}>
         상태 및 이펙트 학습하기
       </h2>
-      <h3>글자 두께 설정</h3>     
-      <div role="group" className="bg-stone-200 py-1 px-4 rounded-full shadow-lg shadow-indigo-300/40">
-        {
-          fontWeights.map((weight, index) => {
-            const isActive = weight === selectedFontWeight
-            return (
-              <button key={weight} type="button" 
-                onClick={() => handleChangeFontWeight(index)}
-                className={`py-0.5 px-1.5 bg-stone-200 text-stone-600
-                ${isActive ? 'bg-stone-950 text-stone-50 rounded-full' : 'font-normal' } 
-                hover:bg-indigo-100 transition-colors duration-200`}>
-                {weight}
-              </button>
-            )
-          })
-        }
-    </div>
+      {
+        data && data.items?.map(item => (
+          <div key={item.id} className="todo">
+            <strong>{item.doit}</strong>
+          </div>
+        ))
+      }
     </div>
   )
 }
