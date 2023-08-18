c8022ca81408ad7cfca85dbac8ea08e45432d0b1
rustandbone <dlehdgh14@naver.com>
2023-08-18 21:33:26 +0900

    커스텀 훅 - fetch data

---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,47 @@
+// 컴포넌트 간 로직 공유
+// 사용자 정의 훅(함수)를 작성
+
+import { useEffect, useState } from 'react';
+
+export default function useFetchData(endpoint) {
+  // 훅의 규칙(컴포넌트 또는 다른 훅 내부에서만 사용 가능)
+  // custom hook 내부에서 built-in-hook 사용 가능
+
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    //중단(abort) 컨트롤러 생성
+    const controller = new AbortController();
+
+    setIsLoading(true);
+
+    async function fetchProducts() {
+      try {
+        const response = await fetch(endpoint, {
+          signal: controller.signal,
+        });
+        const responseData = await response.json();
+        setData(responseData);
+      } catch (error) {
+        // 통신 중단에 따른 오류가 아니라면 오류 설정
+        if (!(error instanceof DOMException)) {
+          setError(error);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchProducts();
+
+    //Strict Mode(detect impure function component)
+    //mount(1) => unmount => mount(2)
+    return () => {
+      controller.abort();
+    };
+  }, [endpoint]);
+
+  return { data, isLoading, error };
+}
