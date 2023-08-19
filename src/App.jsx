758d1cc4a48f2f1a58e7054e50d939f12cb17557
rustandbone <dlehdgh14@naver.com>
2023-08-19 19:42:08 +0900

    0818 금 router 학습

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,13 @@
-// import Heading from "./components/Heading";
-// import Description from './components/Description';
-// import List from "./components/RenderingProcessList"
+import { createBrowserRouter, RouterProvider, Link, NavLink } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
-import Demo from './pages/Demo';
-import FilterableList from './pages/Practice/parts/FilterableList';
-import Practice from './pages/Practice/Practice';
-import LearnStateAndEffects from './pages/LearnStateAndEffects';
-import ProductList from './pages/ProductList';
+import router from './routes';
 
-// {/* 홈페이지 : index.html */}
-// {/* 소개 페이지 : about.html */}
-// {/* 제품 목록 페이지 : products.html */}
-// {/* 의뢰 페이지 : contact.html */}
-
-//App 함수 컴포넌트를 작성합니다
 function App() {
-  //JSX 값 반환
   return (
     <div className="App">
-      <RootLayout>
-        <LearnStateAndEffects />
-        <ProductList />
-        {/* <Demo /> */}
-        {/* <FilterableList /> */}
-        {/* <Practice /> */}
-      </RootLayout>
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
 export default App;
-
-//  {/* <Heading>React</Heading> */}
-//  <Heading />
-//  {/* <Description>...</Description> */}
-//  <Description />
-//    {/* <List></List> */}
-//  <List />
