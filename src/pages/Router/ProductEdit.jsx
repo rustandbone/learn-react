d100f97186c5141f7d9ba9de7dad30915ada50ed
rustandbone <dlehdgh14@naver.com>
2023-08-22 18:59:37 +0900

    debounce, rest API 부분 구현

--- src/pages/Router/ProductEdit.jsx
+++ src/pages/Router/ProductEdit.jsx
@@ -1,7 +1,9 @@
 import Spinner from "@/components/Spinner";
 import useProductItem from "@/hooks/useProductItem";
 import { useEffect, useState, useId } from "react";
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
+import { useDelete as useDeleteProduct, useUpdate as useUpdateProduct } from "@/hooks/products/useProducts";
+import debounce from "@/utils/debounce";
 
 const initialFormState = {
   title : '',
@@ -15,11 +17,16 @@ export default function ProductEdit() {
   const colorId = useId();
   
   const {productId} = useParams();
+  const navigate = useNavigate();
   const {data, isLoading} = useProductItem(productId);
   const [formState, setFormState] = useState(initialFormState);
 
+  const deleteProduct = useDeleteProduct();
+  const updateProduct = useUpdateProduct();
+
   useEffect(() => {
     if(!isLoading && data) {
+      console.log(data)
       setFormState({
         title: data.title,
         price: data.price,
@@ -34,6 +41,52 @@ export default function ProductEdit() {
       [target.name]: target.value,
     })
   }
+
+  const handleDebounceChangeInput = debounce(({target}) => {
+    setFormState({
+      ...formState,
+      [target.name]: target.value,
+    })
+  })
+
+
+  const handleEditProduct = (e) => {
+    e.preventDefault();
+    updateProduct(productId, formState)
+    .then(() => navigate('/products'))
+    .catch(error => console.log(error));
+
+      /* fetch(`${import.meta.env.VITE_PB_API}/collections/products/records/${productId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formState)
+    })
+    .then(response => console.log(response))
+    .catch(error => {
+      console.log(error);
+    }) */
+  }
+
+  const handleDeleteProduct = () => {
+    const userConfirm = confirm('정말로 확실하게 지우실 건가요?')
+    if(userConfirm) {
+      deleteProduct(productId)
+      .then(() => navigate('/products'))
+      .catch(error => console.log(error))
+      // fetch(`${import.meta.env.VITE_PB_API}/collections/products/records/${productId}`, {
+      //   method: 'DELETE'
+      // })
+      // .then(() => {
+      //   //정상적으로 삭제되면 제품 목록 페이지로 이동
+      //   navigate('/products');
+      // })
+      // .catch(error => {
+      //   console.log(error);
+      // })
+    }
+  }
   
   if(isLoading) {
     return <Spinner size={120} />
@@ -42,16 +95,28 @@ export default function ProductEdit() {
   if(data) {
     return (
       <>
-        <h2 className="text-2xl text-center">{data.title} 수정 폼</h2>
-        <form>
+        <h2 className="text-2xl text-center">{data.title}({data.color}) 수정 폼</h2>
+        <form onSubmit={handleEditProduct}>
           {/* title */}
           <div>
-            <label htmlFor={titleId}>타이틀</label>
-            <input type="text" name="title" id={titleId} value={formState.title} onChange={handleChangeInput} />
+            <label htmlFor={titleId}>제품명</label>
+            <input type="text" name="title" id={titleId} defaultValue={formState.title} onChange={handleDebounceChangeInput} />
           </div>
           {/* photo */}
           {/* color */}
+          <div>
+            <label htmlFor={colorId}>색상</label>
+            <input type="text" name="color" id={colorId} defaultValue={formState.color} onChange={handleDebounceChangeInput} />
+          </div>
           {/* price */}
+          <div>
+            <label htmlFor={priceId}>가격</label>
+            <input type="number" name="price" id={priceId} value={formState.price} onChange={handleChangeInput} />
+          </div>
+          <div>
+            <button type="submit">수정</button>
+            <button type="button" onClick={handleDeleteProduct}>삭제</button>
+          </div>
         </form>
       </>
     )
