99871262a50fdc534e6de1eeb99b02cba4524d84
rustandbone <dlehdgh14@naver.com>
2023-08-23 18:23:27 +0900

    학습 내비게이션 추가

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,13 +1,36 @@
 import HeaderBar from './HeaderBar';
 import FooterBar from './FooterBar';
 import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function RootLayout(props) {
+function RootLayout({displaySideMenu = false}) {
   return ( 
     <>
         <HeaderBar />
-        <main className='p-5'>
-          <Outlet />
+        <main className='flex gap-4 p-5'>
+          {
+            displaySideMenu &&
+            <nav className='bg-slate-100 py-3 px-4 min-w-[100px]'>
+              <h3 className='sr-only'>학습 내비게이션 메뉴</h3>
+              <ul>
+                <li>
+                  <Link to="/learn/01">Props 전달</Link>
+                </li>
+                <li>
+                  <Link to="/learn/02">상태 끌어올리기</Link>
+                </li>
+                <li>
+                  <Link to="/learn/03">속성 드릴링 이슈</Link>
+                </li>
+                <li>
+                  <Link to="/learn/04">컨텍스트 상태 공유 Context API</Link>
+                </li>
+              </ul>
+            </nav>
+          }
+          <div className='flex-1'>
+            <Outlet />
+          </div>
         </main>
         <FooterBar />
     </>
--- src/routes.jsx
+++ src/routes.jsx
@@ -6,14 +6,26 @@ import Contact from './pages/Router/Contact';
 import ProductEdit from './pages/Router/ProductEdit';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import PassingProps from './learn/1-passing-props';
+import LiftingStateUp from './learn/2-lifting-state-up';
+import ReactContextAPI from './learn/4-2-seperation-react-context'
+import PropsDrillingIssue from './learn/3-prop-drilling-issue';
 
 // 구버전처럼 사용할 사용자를 위한 최신 방법
 // 배열 → JSX 
 const router = createBrowserRouter(
   // 유틸리티 함수
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
+    <Route path="/" element={<RootLayout displaySideMenu />}>
       <Route index element={<Home />} />
+      {/* <Route path="learn/01" element={<PassingProps />} />
+      <Route path="learn/02" element={<LiftingStateUp />} /> */}
+      <Route path="learn">
+        <Route path="01" element={<PassingProps />} />
+        <Route path="02" element={<LiftingStateUp />} />
+        <Route path="03" element={<PropsDrillingIssue />} />
+        <Route path="04" element={<ReactContextAPI />} />
+      </Route>
       <Route path="signup" element={<SignUp />} />
       <Route path="signin" element={<SignIn />} />
       <Route path="products" element={<Products />} />
