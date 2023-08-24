c2cf5027b345f1818960f2f168dbf0c891ce2c61
rustandbone <dlehdgh14@naver.com>
2023-08-24 18:41:42 +0900

    0824 목 로그인, 로그아웃 등 학습

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -1,25 +1,39 @@
+import { useAuth } from '@/contexts/Auth';
+import { useNavigate } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
 function Nav() {
+  const { isAuth, signOut } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    await signOut();
+    navigate('/')
+  }
+
   return (
     <nav>
       <ul className="flex gap-4 p-5 font-extralight">
-        <li>
+        {
+          !isAuth && (
+          <li>
+            <NavLink
+              to="/signin"
+              className={({ isActive }) => isActive ? 'font-semibold text-rose-600' : ''}
+            >
+              Sign in
+            </NavLink>
+          </li>
+          )
+        }
+        {/* <li>
           <NavLink
             to="/signup"
             className={({ isActive }) => isActive ? 'font-semibold text-rose-600' : ''}
           >
             Sign up
           </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/signin"
-            className={({ isActive }) => isActive ? 'font-semibold text-rose-600' : ''}
-          >
-            Sign in
-          </NavLink>
-        </li>
+        </li> */}
         <li>
           <NavLink
             to="/products"
@@ -36,6 +50,14 @@ function Nav() {
             Contact
           </NavLink>
         </li>
+        {
+          isAuth && (
+            <button type="button" onClick={handleSignOut}
+              className='py-0.5 px-2 border border-white/40 rounded-md hover:border-white'>
+              Sign Out
+            </button>
+          )
+        }
       </ul>
     </nav>
   );
