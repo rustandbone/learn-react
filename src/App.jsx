40ed9f11e03cf664cda186823f4d7521eb85a7ba
rustandbone <dlehdgh14@naver.com>
2023-08-25 17:46:20 +0900

    HelmetProvider 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import ThemeProvider from './contexts/Theme';
 import AuthProvider from './contexts/Auth';
+import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
-      <ThemeProvider>
-        <AuthProvider>
-            <div className="App">
-              <RouterProvider router={router} />
-            </div>
-        </AuthProvider>
-      </ThemeProvider>
+      <HelmetProvider>
+        <ThemeProvider>
+          <AuthProvider>
+              <div className="App">
+                <RouterProvider router={router} />
+              </div>
+          </AuthProvider>
+        </ThemeProvider>
+      </HelmetProvider>
       <Toaster /> 
     </>
   );
